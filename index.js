0c8cb60061d916ad7b235f6fd0f64850329338f8
danielguirra <daniel.guirra777@gmail.com>
2021-08-27 09:16:43 -0300

    shoter game

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.render('main'));
+app.get('/', (req, res) => res.render('shoter'));
 app.set('view engine', 'ejs')
 
 app.listen(port, () => console.log(`O bot está rodando na porta: http://localhost:${port}`));
